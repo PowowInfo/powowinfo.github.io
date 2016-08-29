3ba500160ecc4e99afb08b89a4543f994b32c59d
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2016-08-29 21:04:05 +0200

    Materiaze CSS theme website

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.carousel').carousel({
+            full_width: true,
+            time_constant: 100
+        });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
